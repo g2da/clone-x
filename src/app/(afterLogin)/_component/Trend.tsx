c5a5d4f94963518c4ff00f1386d83a3d62b045f3
g2g2 <g2g2@ncsoft.com>
2024-09-03 11:58:37 +0900

    chore: rm unused curly braces

--- src/app/(afterLogin)/_component/Trend.tsx
+++ src/app/(afterLogin)/_component/Trend.tsx
@@ -3,7 +3,7 @@ import style from "./_css/trend.module.css";
 
 export default function Trend(): React.JSX.Element {
   return (
-    <Link href={`/search?q=트렌드`} className={style.container}>
+    <Link href="/search?q=트렌드" className={style.container}>
       <div className={style.count}>실시간트렌드</div>
       <div className={style.title}>지투</div>
       <div className={style.count}>1,234 posts</div>
