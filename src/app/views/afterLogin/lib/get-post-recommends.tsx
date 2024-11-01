9b3c760845ea0779c1923a22b6a997c9a937dd42
g2 Hong <hon9g2@gmail.com>
2024-11-01 10:21:49 +0900

    chore: fixme 해결

--- src/app/views/afterLogin/lib/get-post-recommends.tsx
+++ src/app/views/afterLogin/lib/get-post-recommends.tsx
@@ -1,10 +1,16 @@
-export async function getPostRecommends() {
-  const res = await fetch(`http://localhost:9090/api/postRecommends`, {
-    next: {
-      tags: ["posts", "recommends"],
-    },
-    cache: "no-store",
-  });
+interface getPostRecommendsProps {
+  pageParam?: number;
+}
+export async function getPostRecommends({ pageParam }: getPostRecommendsProps) {
+  const res = await fetch(
+    `http://localhost:9090/api/posts/recommends?cursor=${pageParam}`,
+    {
+      next: {
+        tags: ["posts", "recommends"],
+      },
+      cache: "no-store",
+    }
+  );
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
 
@@ -15,26 +21,3 @@ export async function getPostRecommends() {
 
   return res.json();
 }
-
-// FIXME: handler 확인
-// type Props = { pageParam?: number };
-
-// export async function getPostRecommends({ pageParam }: Props) {
-//   const res = await fetch(
-//     `http://localhost:9090/api/posts/recommends?cursor=${pageParam}`,
-//     {
-//       next: {
-//         tags: ["posts", "recommends"],
-//       },
-//     }
-//   );
-//   // The return value is *not* serialized
-//   // You can return Date, Map, Set, etc.
-
-//   if (!res.ok) {
-//     // This will activate the closest `error.js` Error Boundary
-//     throw new Error("Failed to fetch data");
-//   }
-
-//   return res.json();
-// }
--- src/app/views/afterLogin/ui/after-login-home.page.tsx
+++ src/app/views/afterLogin/ui/after-login-home.page.tsx
@@ -1,32 +1,41 @@
 import PostForm from "@/app/(afterLogin)/home/_component/PostForm";
+import Tab from "@/app/(afterLogin)/home/_component/Tab";
 import TabProvider from "@/app/(afterLogin)/home/_component/TabProvider";
 import style from "@/app/(afterLogin)/home/_component/_css/home.module.css";
+import { auth } from "@/auth";
 import {
   dehydrate,
   HydrationBoundary,
   QueryClient,
 } from "@tanstack/react-query";
+import { Metadata } from "next";
 import { Suspense } from "react";
-import TabDeciderSuspense from "./tab-decider-suspense";
-import Loading from "./after-login-home.loading";
-import Tab from "@/app/(afterLogin)/home/_component/Tab";
 import { getPostRecommends } from "../lib/get-post-recommends";
+import Loading from "./after-login-home.loading";
+import TabDeciderSuspense from "./tab-decider-suspense";
+
+export const metadata: Metadata = {
+  title: "홈",
+  description: "홈",
+};
 
 export async function AfterLoginHomePage(): Promise<React.JSX.Element> {
+  const session = await auth();
   const queryClient = new QueryClient();
-  await queryClient.prefetchQuery({
+
+  await queryClient.prefetchInfiniteQuery({
     queryKey: ["posts", "recommends"],
     queryFn: getPostRecommends,
+    initialPageParam: 0,
   });
   const dehydrateState = dehydrate(queryClient);
 
   return (
     <main className={style.main}>
-      {/* FIXME: HydrationBoundary 확인  */}
       <HydrationBoundary state={dehydrateState}>
         <TabProvider>
           <Tab />
-          <PostForm />
+          <PostForm me={session} />
           <Suspense fallback={<Loading />}>
             <TabDeciderSuspense />
           </Suspense>
